708e211e1c9c2e0b4afc9af51bbc9ab35a621ac1
Ruben Verborgh <ruben@verborgh.org>
2012-11-29 13:02:56 +0100

    Add entity extraction.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Dummy Named-Entity Recognition server
 This server can help you test tools that depend on external named-entity recognition services.
+
+# Usage
+
+    $ ./server.js 4500 &
+    $ curl -X POST --data "Dummy entity extraction is cool." localhost:4500
+      # ["extraction","entity","Dummy"]
--- server.js
+++ server.js
@@ -2,12 +2,33 @@
 var express = require('express'),
     fs = require('fs'),
     md = require("node-markdown").Markdown;
+
+var maxEntityCount = 3;
+
 var app = express();
 
+// Show a readme file
 app.get('/', function(req, res) {
   fs.readFile('README.md', 'utf-8', function (error, markdown) {
     res.send(md(markdown));
   });
 });
+
+// Extract the entities from the request body
+app.post('/', function(req, res) {
+  var text = '';
+  req.setEncoding('utf-8');
+  req.on('data', function (data) { text += data; });
+  req.on('end', function () {
+    res.send(JSON.stringify(extractEntities(text)));
+  });
+});
+
+  // Extract the `maxEntityCount` longest words
+function extractEntities(text) {
+  return text.split(/[ \t\n\r\.\?!,;]+/)
+             .sort(function (a, b) { return b.length - a.length; })
+             .slice(0, maxEntityCount);
+}
 
 app.listen(process.argv[2] || 3000);
